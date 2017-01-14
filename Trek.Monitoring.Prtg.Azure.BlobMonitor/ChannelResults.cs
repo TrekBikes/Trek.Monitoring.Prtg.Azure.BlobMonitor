657da8d7a49e49d2b1f9d63c38e9f9bb63e4eba3
Chad Kittel <chad.kittel@gmail.com>
2017-01-14 10:51:08 -0600

    Upgrade azure storage library and clean up nugat refs

--- Trek.Monitoring.Prtg.Azure.BlobMonitor/ChannelResults.cs
+++ Trek.Monitoring.Prtg.Azure.BlobMonitor/ChannelResults.cs
@@ -3,6 +3,7 @@ namespace Trek.Monitoring.Prtg.Azure.BlobMonitor
     public class ChannelResults
     {
         public long TotalNumberOfBlockBlobs { get; set; } = 0;
+
         public long TotalContainerSizeInBytes { get; set; } = 0;
 
         public long TotalNumberOfMatchedBlockBlobs { get; set; } = 0;
